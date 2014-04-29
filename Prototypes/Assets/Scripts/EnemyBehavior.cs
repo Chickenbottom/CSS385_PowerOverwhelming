9f6d2fa1a102949f67124de190af1772e6d2616a
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-04-29 05:49:21 -0700

    Initial commit , level selector and Multiple Units
    The 'Fixed Angle' property on Rigid Body 2d allows the unity physics
    engine to take care of collisions (properly pushing sprites out of the
    way) without rotating the sprite.

---
+++ Prototypes/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	private Vector3 mTarget;
+	private float mSpeed = 20f;
+	private float mRotationSpeed = 0f;
+	private float mAngle = 0f;
+	
+	
+	private bool isAtTarget = false;
+	
+	private float mRotationRate = 0.8f;
+	// Use this for initialization
+	void Start () {
+		mTarget = new Vector3(80, 0, 0);
+		
+		Vector3 initialDir = mTarget;
+		initialDir.Normalize();
+		//this.transform.up = initialDir;
+		mSpeed = Random.Range(mSpeed * .8f, mSpeed * 1.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//RotateTowards(mTarget);
+		if (isAtTarget)
+			return;
+		
+		UpdateMovement(mSpeed);
+		
+		if (Vector3.Distance(this.transform.position, mTarget) < 1.0f)
+			isAtTarget = true;
+		
+		//CheckCollision();
+	}
+	
+	void OnCollisionEnter()
+	{
+		this.transform.up = new Vector3(0, 1, 0);
+		Debug.Log ("Collision");
+	}
+	
+	private void CheckCollision()
+	{
+		World world = GameObject.Find ("World").GetComponent<World>();
+		
+		float overlap;
+		World.WorldBoundStatus status =
+			world.ObjectCollideWorldBound(this.transform.position, this.collider2D, out overlap);
+		
+		if (status != World.WorldBoundStatus.Inside) {
+			ChangeDirection();
+		}	
+		
+		switch (status) 
+		{
+		case(World.WorldBoundStatus.CollideRight):
+		case(World.WorldBoundStatus.CollideLeft):
+			transform.Translate (overlap, 0, 0, Space.World);
+			break;
+		case(World.WorldBoundStatus.CollideTop):
+		case(World.WorldBoundStatus.CollideBottom):
+			transform.Translate(0, overlap, 0, Space.World);
+			break;
+		}
+	}
+	
+	private void ChangeDirection()
+	{
+		Vector2 r = Random.insideUnitCircle;
+		transform.up = new Vector3 (0, r.y, 0);
+	}
+	
+	public void UpdateMovement(float speed)
+	{
+		Vector3 targetDir = mTarget - transform.position;
+		targetDir.Normalize();
+		
+		speed *= Time.smoothDeltaTime;
+		
+		transform.position += speed * targetDir;
+	}
+	
+	private void RotateTowards(Vector3 target) {
+		
+		Vector3 targetDir = target - transform.position;
+		targetDir.Normalize();
+		
+		float theta = Mathf.Acos(Vector3.Dot(transform.up, targetDir));
+		theta *= Mathf.Rad2Deg;
+		
+		
+		if (theta > 0.001f)
+		{ // if not already in the same direction
+			
+			Vector3 myDir3 = transform.up;
+			Vector3 targetDir3 = targetDir;
+			Vector3 sign = Vector3.Cross(myDir3, targetDir3);
+			
+			float angleToTarget = Mathf.Sign(sign.z) * theta;
+			//float turnRate = Mathf.Abs(angleToTarget) > kMaxRotationSpeed ? kMaxRotationSpeed * Mathf.Sign(sign.z) : angleToTarget;
+			float turnRate = angleToTarget * mRotationRate;
+			mRotationSpeed = turnRate;
+			mAngle = theta;
+		}
+	}
+	
+}
---
+++ Prototypes/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿//C#
+using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+	
+	private const int kButtonMargin = 20;
+	private const int kButtonWidth = 250;
+	
+	private string kMenuTitle = "Power Overwhelming Prototypes";
+	
+	void OnGUI () 
+	{
+		// Make a background box
+		GUI.Box(new Rect(10, 10, kButtonWidth + kButtonMargin, 90), kMenuTitle);
+		
+		CreateLevelButton("Multiple Unit Movement", "MultipleUnitMovement");
+	}
+	
+	public void CreateLevelButton(string levelName, string sceneName)
+	{
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20, 40, kButtonWidth, 20), levelName)) {
+			Application.LoadLevel(sceneName);
+		}
+	
+	}
+}
