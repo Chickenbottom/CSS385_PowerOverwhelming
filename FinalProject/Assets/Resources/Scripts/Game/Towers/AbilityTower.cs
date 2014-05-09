f67540f5b5c31df90f0c30c2d17569519b016e56
Chickenbottom <chad.hickenbottom@gmail.com>
2014-05-09 13:35:10 -0700

    Added Tower behavior files. Put them in a tower folder. Added tower Prefabs.

---
+++ FinalProject/Assets/Resources/Scripts/Game/Towers/AbilityTower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityTowerBehavior: TowerBehavior {
+
+	const float kHealRate = 50;
+	float mHealBonus = 1;
+	public TOWERTYPE mTowerType;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+		setSelected(this);
+	}
+	public void setHealBonus(float bonus = 1){
+		mHealBonus = bonus;
+	}
+	public float getHealRate(){
+		return kHealRate * mHealBonus;
+	}
+	public TOWERTYPE getMyTowerType(){
+		return mTowerType;
+	}
+
+}
