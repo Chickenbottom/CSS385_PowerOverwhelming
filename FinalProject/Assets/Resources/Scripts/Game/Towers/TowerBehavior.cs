5479c13074999133521c3b5b2a186f60ba17d042
James Murphree <jamesus95@gmail.com>
2014-05-11 00:36:30 -0700

    started tower class

--- FinalProject/Assets/Resources/Scripts/Game/Towers/TowerBehavior.cs
+++ FinalProject/Assets/Resources/Scripts/Game/Towers/TowerBehavior.cs
@@ -1,75 +1,90 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TowerBehavior : MonoBehaviour {
+public abstract class TowerBehavior : MonoBehaviour {
 
-	//abstract void Click();
-	//abstract void getMyTowerType();
+    ////abstract void Click();
+    ////abstract void getMyTowerType();
 
-	public enum TOWERTYPE{
-		MELEE,
-		RANGED,
-		MAGIC,
-		HEAL,
-		NONE,
-	};
+    //public enum TOWERTYPE{
+    //    MELEE,
+    //    RANGED,
+    //    MAGIC,
+    //    HEAL,
+    //    NONE,
+    //};
 
-	AbilityTowerBehavior abilityTower = null;
-	UnitSpawnTower spawnTower = null;
-	TOWERTYPE selectedTower;
-	// Use this for initialization
-	void Start () {
-		selectedTower = TOWERTYPE.NONE;
-	}
+    //AbilityTowerBehavior abilityTower = null;
+    //UnitSpawnTower spawnTower = null;
+    //TOWERTYPE selectedTower;
+    //// Use this for initialization
+    //void Start () {
+    //    selectedTower = TOWERTYPE.NONE;
+    //}
 	
-	// Update is called once per frame
-	void Update () {
+    //// Update is called once per frame
+    //void Update () {
 
-		if(Input.GetMouseButtonUp(1) ){
-			if(selectedTower != TOWERTYPE.NONE && 
-			   selectedTower != TOWERTYPE.HEAL && 
-			   spawnTower != null){
-				Vector2 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-				spawnTower.setDestination(destination);
-				clearSelected();
-			}
-		}
+    //    if(Input.GetMouseButtonUp(1) ){
+    //        if(selectedTower != TOWERTYPE.NONE && 
+    //           selectedTower != TOWERTYPE.HEAL && 
+    //           spawnTower != null){
+    //            Vector2 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //            spawnTower.setDestination(destination);
+    //            clearSelected();
+    //        }
+    //    }
 
-		if(Input.GetKey(KeyCode.A)){
+    //    if(Input.GetKey(KeyCode.A)){
 		
-			if(spawnTower != null){
-				spawnTower.hitTower(50f);
-			}
-			if(abilityTower != null){
-				abilityTower.hitTower(50f);
-			}
-		}
-	}
-	public void setSelected(UnitSpawnTower ST = null, AbilityTowerBehavior AT = null){
+    //        if(spawnTower != null){
+    //            spawnTower.hitTower(50f);
+    //        }
+    //        if(abilityTower != null){
+    //            abilityTower.hitTower(50f);
+    //        }
+    //    }
+    //}
+    //public void setSelected(UnitSpawnTower ST = null, AbilityTowerBehavior AT = null){
 
 		
-		if(selectedTower == TOWERTYPE.HEAL)
-			if(ST != null){
-				spawnTower = ST;
-				spawnTower.healTower(abilityTower.getHealRate());
-				clearSelected();
-		}
+    //    if(selectedTower == TOWERTYPE.HEAL)
+    //        if(ST != null){
+    //            spawnTower = ST;
+    //            spawnTower.healTower(abilityTower.getHealRate());
+    //            clearSelected();
+    //    }
 
-		clearSelected();
-		if(selectedTower == TOWERTYPE.NONE){
-			if(AT == null){
-				spawnTower = ST;
-				selectedTower = ST.getMyTowerType();
-			}
-			else{
-				abilityTower = AT;
-				selectedTower = AT.getMyTowerType();
-			}
-		}
-	}
-	public void clearSelected(){
-		selectedTower = TOWERTYPE.NONE;
-		abilityTower = null;
-		spawnTower = null;
-	}
+    //    clearSelected();
+    //    if(selectedTower == TOWERTYPE.NONE){
+    //        if(AT == null){
+    //            spawnTower = ST;
+    //            selectedTower = ST.getMyTowerType();
+    //        }
+    //        else{
+    //            abilityTower = AT;
+    //            selectedTower = AT.getMyTowerType();
+    //        }
+    //    }
+    //}
+    //public void clearSelected(){
+    //    selectedTower = TOWERTYPE.NONE;
+    //    abilityTower = null;
+    //    spawnTower = null;
+    //}
+
+    abstract void Click();
+    abstract void GetTowerType();
+    abstract void Start();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
 }
