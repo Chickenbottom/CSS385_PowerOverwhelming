bb97dd49dfead979020749bcf5d43ab98c989710
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-05-12 03:04:12 -0700

    Integrated Scene Transition

---
+++ src/Assets/Resources/SceneTransition/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void OnGUI()
+	{
+		if (GUI.Button(new Rect(120, 35, 150, 50), "Return to Level Selector"))
+			Application.LoadLevel("LevelLoader");
+			
+		GUI.Label(new Rect(135, 65, 150, 50), "Left-Alt to spawn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+			Debug.Log (GameState.Score);
+			
+		if (Input.GetButtonDown("Fire2"))
+			GameState.AddToScore(5);
+	}
+}
---
+++ src/Assets/Resources/SceneTransition/GameState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState {
+	private static int mGameLevel;
+	private static int[] mNumEnemies;
+	
+	public static int Score { get { return mScore; } }
+	private static int mScore = 0;
+	
+	public static int MageSquadCount { get; set; }
+	public static int SwordsmanSquadCount { get; set; }
+	public static int ArcherSquadCount { get; set; }
+	
+	static GameState()
+	{
+		MageSquadCount = 4;
+		SwordsmanSquadCount = 4;
+		ArcherSquadCount = 4;
+	}
+	
+	public static void AddToScore(int value)
+	{
+		mScore += value;
+	}
+	
+	public static void LoadLevel(int level)
+	{
+		//mGameLevel = level;
+		Application.LoadLevel("Level"+level.ToString());
+	}
+}
+
