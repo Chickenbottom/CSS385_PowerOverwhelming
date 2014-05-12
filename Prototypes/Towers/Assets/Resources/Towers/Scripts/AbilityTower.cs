f007d7730633acad9e2a9c424db1364ab71d9c54
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-05-12 01:44:22 -0700

    Added James' Tower Prototype

---
+++ Prototypes/Towers/Assets/Resources/Towers/Scripts/AbilityTower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityTower: Tower {
+
+    public override void Click()
+    {
+        Debug.Log("YAY IT WORKED" + health + "  " + type);
+    }
+
+    void Start()
+    {
+        type = TowerType.kAbility;
+        health = 100;
+    }
+
+}
