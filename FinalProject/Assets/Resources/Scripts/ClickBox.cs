5557041e19a5a2b607200e85a77aa98b2446731d
James Murphree <jamesus95@gmail.com>
2014-05-13 14:27:30 -0700

    mouse stuff

---
+++ FinalProject/Assets/Resources/Scripts/ClickBox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickBox : MonoBehaviour {
+
+    private Bounds mClickBox;
+
+	void Start () {
+        BoxCollider2D temp = GameObject.Find("ClickBox").GetComponent<BoxCollider2D>();
+        mClickBox = new Bounds(new Vector3(temp.center.x, temp.center.y, 0f), new Vector3(temp.size.x, temp.size.y, 0f));
+        Debug.Log(mClickBox);
+	}
+	
+	void Update () {
+	
+	}
+
+    public Bounds GetClickBocBounds()
+    {
+        return mClickBox;
+    }
+}
