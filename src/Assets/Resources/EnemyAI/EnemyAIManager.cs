159187d522cf9effd3e114f5b1fed336286ebacb
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-05-15 07:45:44 -0700

    Fixed MageWaypoint, balance changes
    Fixed - Enemy squads heading to the mage tower are getting stuck on the
    wall.

--- src/Assets/Resources/EnemyAI/EnemyAIManager.cs
+++ src/Assets/Resources/EnemyAI/EnemyAIManager.cs
@@ -54,9 +54,9 @@ public class EnemyAIManager : MonoBehaviour
 		units = new List<EnemySquad> ();
 		
 		wayPoints = new Dictionary<Waypoints, Vector3>();
-		wayPoints.Add (Waypoints.AbilityTower, new Vector3(46, -40, 0));
+		wayPoints.Add (Waypoints.AbilityTower, new Vector3(43, -40, 0));
 		wayPoints.Add (Waypoints.ArcherTower, new Vector3(-105, -40, 0));
-		wayPoints.Add (Waypoints.MageTower, new Vector3(45, 50, 0));
+		wayPoints.Add (Waypoints.MageTower, new Vector3(43, 50, 0));
 		wayPoints.Add (Waypoints.SwordsmanTower, new Vector3(-108, 50, 0));
 		wayPoints.Add (Waypoints.King, new Vector3(-33, 56, 0));
 		wayPoints.Add (Waypoints.Center, new Vector3(-33, 10, 0));
@@ -104,6 +104,11 @@ public class EnemyAIManager : MonoBehaviour
 		es = new EnemySquad (4, wayPoints[Waypoints.LowerCenter]);
 		es.AddWaypoint(wayPoints[Waypoints.King]);
 		units.Add (es);
+		
+		es = new EnemySquad (6, wayPoints[Waypoints.LowerCenter]);
+		es.AddWaypoint(wayPoints[Waypoints.ArcherTower]);
+		es.AddWaypoint(wayPoints[Waypoints.King]);
+		units.Add (es);
 	}
 	
 	void SpawnWave2()
@@ -116,7 +121,7 @@ public class EnemyAIManager : MonoBehaviour
 		
 		es = new EnemySquad (5, wayPoints[Waypoints.Center]);
 		es.AddWaypoint(wayPoints[Waypoints.MageTower]);
-		es.AddWaypoint(wayPoints[Waypoints.Center]);
+		es.AddWaypoint(wayPoints[Waypoints.King]);
 		units.Add (es);
 	}
 	
@@ -130,8 +135,7 @@ public class EnemyAIManager : MonoBehaviour
 		units.Add (es);
 		
 		es = new EnemySquad (7, wayPoints[Waypoints.Center]);
-		es.AddWaypoint(wayPoints[Waypoints.MageTower]);
-		es.AddWaypoint(wayPoints[Waypoints.Center]);
+		es.AddWaypoint(wayPoints[Waypoints.MageTower]);;
 		es.AddWaypoint(wayPoints[Waypoints.King]);
 		units.Add (es);
 		
