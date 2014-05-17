4d43379fd3f9d4bb3eb43e1ffdb8bc324b7c6ba0
Rodelle Ladia Jr <rLadia@gmail.com>
2014-05-17 01:06:07 -0700

    Added ability to create AI from text files

--- src/Assets/Resources/EnemyAI/EnemyAI.cs
+++ src/Assets/Resources/EnemyAI/EnemyAI.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.IO;
 
 public enum Waypoint
 {
@@ -11,10 +12,9 @@ public enum Waypoint
     SwordsmanTower,
     MageTower,
     Center,
-    LowerLeft,
-    LowerCenter,
-    LowerRight,
-    SpawnPoint,
+    SpawnLeft,
+    SpawnCenter,
+    SpawnRight,
 }
 
 public enum SquadPreset
@@ -31,6 +31,12 @@ public enum SquadSize
     Large = 7,
 }
 
+public enum SquadBehavior
+{
+    AttackMove,
+    ForceMove,
+}
+
 public static class EnumHelper
 {    
     public static T FromString<T> (string value)
@@ -42,7 +48,6 @@ public static class EnumHelper
 public class EnemyAI : MonoBehaviour
 {
     public Dictionary<Waypoint, Vector3> Waypoints;
-    public int NumWaves = 3;
 	
     ///////////////////////////////////////////////////////////////////////////////////
     // Unity Overrides
@@ -50,9 +55,9 @@ public class EnemyAI : MonoBehaviour
 	
     void Update ()
     {
-        if (mWaveSpawnInterval < Time.time - mLastEnemySpawn) {
-            mLastEnemySpawn = Time.time;
-            this.SpawnWave ();
+        if (mCurrentWave <= mMaxWaves && Time.time > mWaveSpawnTime) {
+            mCurrentWave ++;
+            this.SpawnWave (mCurrentWave);
         }
 		
         for (int i = units.Count - 1; i >= 0; --i) {
@@ -62,20 +67,20 @@ public class EnemyAI : MonoBehaviour
                 units [i].Update (Time.deltaTime);
         }
 		
-        if (mCurrentWave >= NumWaves && units.Count == 0) {
+        if (mCurrentWave > mMaxWaves && units.Count == 0) {
             GameState.TriggerWin ();
         }
 		
         if (Input.GetButtonDown ("Fire1")) {
-            SpawnWave2 ();
+            SpawnWave (3);
         }
     }
 	
     void Start ()
     {
-        mLastEnemySpawn = Time.time;
         units = new List<EnemySquad> ();
 		
+        // TODO grab these locations from the map
         Waypoints = new Dictionary<Waypoint, Vector3> ();
         Waypoints.Add (Waypoint.AbilityTower, new Vector3 (43, -45, 0));
         Waypoints.Add (Waypoint.ArcherTower, new Vector3 (-105, -45, 0));
@@ -83,14 +88,17 @@ public class EnemyAI : MonoBehaviour
         Waypoints.Add (Waypoint.SwordsmanTower, new Vector3 (-108, 50, 0));
         Waypoints.Add (Waypoint.King, new Vector3 (-33, 56, 0));
         Waypoints.Add (Waypoint.Center, new Vector3 (-33, 10, 0));
-        Waypoints.Add (Waypoint.LowerLeft, new Vector3 (-44, -37, 0));
-        Waypoints.Add (Waypoint.LowerCenter, new Vector3 (-33, -37, 0));
-        Waypoints.Add (Waypoint.LowerRight, new Vector3 (-22, -37, 0));
-        Waypoints.Add (Waypoint.SpawnPoint, new Vector3 (-33, -64, 0));
+        Waypoints.Add (Waypoint.SpawnLeft, new Vector3 (-44, -37, 0));
+        Waypoints.Add (Waypoint.SpawnCenter, new Vector3 (-33, -37, 0));
+        Waypoints.Add (Waypoint.SpawnRight, new Vector3 (-22, -37, 0));
 		
-        SpawnWave ();
+        string aiData = "Assets/Resources/EnemyAI/AI_Level";
+        aiData += GameState.CurrentLevel.ToString();
+        aiData += ".txt";
+        ReadWavesFromFile(aiData);
         
-        this.AddSquad ("Large Default 3.5 Center,King");
+        mCurrentWave = 1;
+        this.SpawnWave(mCurrentWave);
     }
 
     ///////////////////////////////////////////////////////////////////////////////////
@@ -101,31 +109,49 @@ public class EnemyAI : MonoBehaviour
     ///////////////////////////////////////////////////////////////////////////////////
     // Private Methods and Variables
     ///////////////////////////////////////////////////////////////////////////////////	
-    private float mWaveSpawnInterval = 15.0f;
-    private float mLastEnemySpawn;
+    private float mWaveSpawnTime;
     private int mCurrentWave = 0;
-	
+    
+    private int mMaxWaves;
+    List<string>[] mEnemyWaves;
+    float[] mWaveTimers;
+    
     private List<EnemySquad> units;
 	    
-    // Format: <Size> <Preest> <Spawn Time> <Action Type> <Waypoint>,<Waypoint>,...
-    // "Large Default 2.5 Move ArcherTower,SwordsmanTower"
-    // "Individual Elite 1 ForcedMove AbilityTower"
+    private void SpawnWave (int waveNumber)
+    {   
+        if (waveNumber > mMaxWaves)
+            return;
+        
+        Debug.Log ("Spawning Enemy Wave " + waveNumber + " for " + mWaveTimers[waveNumber] + " seconds.");
+        mWaveSpawnTime = Time.time + mWaveTimers[waveNumber];
+        
+        foreach (string s in mEnemyWaves[waveNumber])
+            this.AddSquad(s);
+    }
+    
+    // Format: <Spawn Time> <Size> <Preset> <Action Type> <Waypoint>,<Waypoint>,...
+    // "2.5 Large Default AttackMove ArcherTower,SwordsmanTower"
+    // "1 Individual Elite ForcedMove AbilityTower"
     private void AddSquad (string input)
     {
         SquadPreset preset;
         SquadSize size;
+        SquadBehavior behavior;
+        
         float spawnTime;
         
         char[] delimiters = { ' ', ',' };
-        string[] param = input.Split (delimiters);
+        string[] param = input.Split (delimiters, StringSplitOptions.RemoveEmptyEntries);
         
-        size = EnumHelper.FromString<SquadSize> (param [0]);
-        preset = EnumHelper.FromString<SquadPreset> (param [1]);
-        spawnTime = float.Parse (param [2]);
+        spawnTime = float.Parse (param [0]);
+        size = EnumHelper.FromString<SquadSize> (param [1]);
+        preset = EnumHelper.FromString<SquadPreset> (param [2]);
+        behavior = EnumHelper.FromString<SquadBehavior> (param [3]);
         
         EnemySquad es = new EnemySquad ((int)size, spawnTime);
         
-        for (int i = 3; i < param.Length; ++i) {
+        for (int i = 4; i < param.Length; ++i) {
             Waypoint wp = EnumHelper.FromString<Waypoint> (param [i]);
             es.AddWaypoint (Waypoints [wp]);
         }
@@ -134,55 +160,42 @@ public class EnemyAI : MonoBehaviour
         units.Add (es);
     }
     
-    private void SpawnWave ()
+    private void ReadWavesFromFile(string filepath)
     {
-        mCurrentWave ++;
-        switch (mCurrentWave) {
-        case(1):
-            SpawnWave1 ();
-            break;
-        case(2):
-            //SpawnWave2 ();
-            break;
-        case(3):
-            //SpawnWave3 ();
-            break;
+        StreamReader file = null;
+        string input;
+        try {
+            file = new StreamReader(filepath);
+            input = file.ReadLine();
+            mMaxWaves = int.Parse (input);
+            mEnemyWaves = new List<string>[mMaxWaves + 1]; // 0 based array
+            mWaveTimers = new float[mMaxWaves + 1];
+            
+        } catch (System.Exception e) {
+            Debug.LogError(e.ToString());
         }
-    }
-    
-    // TODO fix this hard coding of enemy waves
-    void SpawnWave1 ()
-    {
-        this.AddSquad ("Large Default 0 LowerLeft,ArcherTower");
-        this.AddSquad ("Medium Default 1");
-        this.AddSquad ("Large Default 3 LowerRight,AbilityTower");
-        this.AddSquad ("Medium Default 4");
-        this.AddSquad ("Medium Default 5 LowerRight,MageTower");
-        this.AddSquad ("Large Default 6 LowerLeft,ArcherTower");
-        this.AddSquad ("Large Default 7");
-    }
-	
-    void SpawnWave2 ()
-    {
-        this.AddSquad ("Individual Default 0 LowerLeft,ArcherTower,SwordsmanTower");
-        this.AddSquad ("Individual Default 0 LowerLeft,ArcherTower,SwordsmanTower");
-        this.AddSquad ("Individual Default 0 LowerLeft,ArcherTower,SwordsmanTower");
         
-        this.AddSquad ("Large Default 1 LowerLeft,ArcherTower,SwordsmanTower");
-        this.AddSquad ("Large Default 2 LowerCenter,Center,MageTower");
-        this.AddSquad ("Large Default 6 LowerCenter,SwordsmanTower");
-        this.AddSquad ("Large Default 7 LowerRight,MageTower");
-    }
-	
-    void SpawnWave3 ()
-    {
-        this.AddSquad ("Medium Default 0 LowerLeft,ArcherTower");
-        this.AddSquad ("Medium Default 1 LowerRight,AbilityTower");
-        this.AddSquad ("Medium Default 2 LowerCenter,MageTower");
-        this.AddSquad ("Medium Default 3 Center,SwordsmanTower");
-        this.AddSquad ("Medium Default 4 LowerLeft,ArcherTower");
-        this.AddSquad ("Medium Default 5 LowerRight,AbilityTower");
-        this.AddSquad ("Medium Default 6 LowerCenter,MageTower");
-        this.AddSquad ("Medium Default 7 Center,SwordsmanTower");
+        int waveNumber = 0;
+        char[] delimiters = { ' ', ',' };
+        
+        string[] waveData;
+        while(true) {
+            input = file.ReadLine();
+            if(file.EndOfStream)
+                break;
+            
+            if (input == "" || input[0] == '#') // ignore comments and blank lines
+                continue;
+                
+            // @EnemyWave <WaveNumber> <WaveTimer>
+            if(input[0] == '@') { // start of new enemy wave
+                waveNumber ++;
+                waveData = input.Split(delimiters);
+                mWaveTimers[waveNumber] = float.Parse(waveData[1]);
+                mEnemyWaves[waveNumber] = new List<string>();
+            } else { // information about the enemy wave
+                mEnemyWaves[waveNumber].Add(input);
+            }
+        }
     }
 }
