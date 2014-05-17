29c0a6bf26ce0b3a75f960b2dbf724e95401e274
Rodelle Ladia Jr <rLadia@gmail.com>
2014-05-16 23:06:23 -0700

    Added stronger enemy waves

--- src/Assets/Resources/EnemyAI/EnemyAI.cs
+++ src/Assets/Resources/EnemyAI/EnemyAI.cs
@@ -45,6 +45,18 @@ public static class EnumHelper
     }
 }
 
+public enum SquadPreset {
+    Default, // all peasants
+    Elite,   // with an elite
+}
+
+public enum SquadSize {
+    Individual,
+    Small,
+    Medium,
+    Large,
+}
+
 public class EnemyAI : MonoBehaviour
 {
     public Dictionary<Waypoint, Vector3> Waypoints;
@@ -71,15 +83,23 @@ public class EnemyAI : MonoBehaviour
             GameState.TriggerWin ();
         }
 		
+<<<<<<< HEAD
         if (Input.GetButtonDown ("Fire1")) {
             SpawnWave (3);
         }
     }
+=======
+		if (Input.GetButtonDown("Fire1")) {
+			SpawnWave2 ();
+		}
+	}
+>>>>>>> Added stronger enemy waves
 	
     void Start ()
     {
         units = new List<EnemySquad> ();
 		
+<<<<<<< HEAD
         // TODO grab these locations from the map
         Waypoints = new Dictionary<Waypoint, Vector3> ();
         Waypoints.Add (Waypoint.AbilityTower, new Vector3 (43, -45, 0));
@@ -100,11 +120,28 @@ public class EnemyAI : MonoBehaviour
         mCurrentWave = 1;
         this.SpawnWave(mCurrentWave);
     }
+=======
+        Waypoints = new Dictionary<Waypoint, Vector3>();
+        Waypoints.Add (Waypoint.AbilityTower, new Vector3(43, -45, 0));
+        Waypoints.Add (Waypoint.ArcherTower, new Vector3(-105, -45, 0));
+        Waypoints.Add (Waypoint.MageTower, new Vector3(43, 50, 0));
+        Waypoints.Add (Waypoint.SwordsmanTower, new Vector3(-108, 50, 0));
+        Waypoints.Add (Waypoint.King, new Vector3(-33, 56, 0));
+        Waypoints.Add (Waypoint.Center, new Vector3(-33, 10, 0));
+        Waypoints.Add (Waypoint.LowerCenter, new Vector3(-33, -37, 0));
+        Waypoints.Add (Waypoint.SpawnPoint, new Vector3(-33, -64, 0));
+		
+		SpawnWave();
+        
+        this.AddSquad("Large Default 3.5 Center,King");
+	}
+>>>>>>> Added stronger enemy waves
 
     ///////////////////////////////////////////////////////////////////////////////////
     // Public Methods and Variables
     ///////////////////////////////////////////////////////////////////////////////////	
 	
+<<<<<<< HEAD
 	
     ///////////////////////////////////////////////////////////////////////////////////
     // Private Methods and Variables
@@ -197,5 +234,112 @@ public class EnemyAI : MonoBehaviour
                 mEnemyWaves[waveNumber].Add(input);
             }
         }
+=======
+	private void SpawnWave()
+	{
+		currentWave ++;
+		switch (currentWave)
+		{
+		case(1):
+			SpawnWave1();
+			break;
+		case(2):
+			SpawnWave2();
+			break;
+		case(3):
+			SpawnWave3();
+			break;
+		}
+	}
+    
+    // Format: <Size> <Preest> <Spawn Time> <Action Type> <Waypoint>,<Waypoint>,...
+    // "Large Default 2.5 Move ArcherTower,SwordsmanTower"
+    // "Individual Elite 1 ForcedMove AbilityTower"
+    public void AddSquad(string input)
+    {
+        SquadPreset preset;
+        SquadSize size;
+        float spawnTime;
+        
+        char[] delimiters = { ' ', ',' };
+        string[] param = input.Split(delimiters);
+        
+        size = EnumHelper.FromString<SquadSize>(param[0]);
+        preset = EnumHelper.FromString<SquadPreset>(param[1]);
+        spawnTime = float.Parse(param[2]);
+        
+        EnemySquad es = new EnemySquad (GetNumMembers(size), Waypoints[Waypoint.LowerCenter], spawnTime);
+        
+        for(int i = 3; i < param.Length; ++i) {
+            Waypoint wp = EnumHelper.FromString<Waypoint>(param[i]);
+            es.AddWaypoint(Waypoints[wp]);
+        }
+        es.AddWaypoint(Waypoints[Waypoint.King]);
+        
+        units.Add(es);
+    }
+     
+    private int GetNumMembers(SquadSize squadSize)
+    {
+        switch (squadSize) {
+        case (SquadSize.Individual):
+            return 1;
+            
+        case(SquadSize.Small):
+            return 2;
+                
+        case(SquadSize.Medium):
+            return 4;
+            
+        case (SquadSize.Large):
+            return 7;
+        }
+        
+        return 0;
+    }
+	
+	// TODO fix this hard coding of enemy waves
+	void SpawnWave1()
+	{
+        this.AddSquad("Large Default 0 ArcherTower");
+        this.AddSquad("Medium Default 1");
+        this.AddSquad("Large Default 3 AbilityTower");
+        this.AddSquad("Medium Default 4");
+        this.AddSquad("Medium Default 5 MageTower");
+        this.AddSquad("Large Default 6 ArcherTower");
+        this.AddSquad("Large Default 7");
+	}
+	
+	void SpawnWave2()
+	{
+        this.AddSquad("Individual Default 0 ArcherTower,SwordsmanTower");
+        this.AddSquad("Individual Default 0 ArcherTower,SwordsmanTower");
+        this.AddSquad("Individual Default 0 ArcherTower,SwordsmanTower");
+        
+        this.AddSquad("Large Default 1 ArcherTower,SwordsmanTower");
+        this.AddSquad("Large Default 2 Center,MageTower");
+        this.AddSquad("Large Default 6 SwordsmanTower");
+        this.AddSquad("Large Default 7 MageTower");
+	}
+	
+	void SpawnWave3()
+	{
+        this.AddSquad("Medium Default 0 ArcherTower");
+        this.AddSquad("Medium Default 1 AbilityTower");
+        this.AddSquad("Medium Default 2 MageTower");
+        this.AddSquad("Medium Default 3 SwordsmanTower");
+        this.AddSquad("Medium Default 4 ArcherTower");
+        this.AddSquad("Medium Default 5 AbilityTower");
+        this.AddSquad("Medium Default 6 MageTower");
+        this.AddSquad("Medium Default 7 SwordsmanTower");
+	}
+}
+
+public static class EnumHelper
+{    
+    public static T FromString<T>(string value)
+    {
+        return (T) Enum.Parse(typeof(T),value);
+>>>>>>> Added stronger enemy waves
     }
 }
