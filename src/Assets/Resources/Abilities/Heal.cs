427e38d2e2aaf947d3a4026c15f1a40515fd95db
James Murphree <jamesus95@gmail.com>
2014-05-20 12:41:51 -0700

    Added heal cooldown in from my branch;

--- src/Assets/Resources/Abilities/Heal.cs
+++ src/Assets/Resources/Abilities/Heal.cs
@@ -4,19 +4,34 @@ using System.Collections;
 public class Heal : Ability
 {
     private int mTarget = -1;
+    private bool mReadyToUse = true;
+    private float mCoolDown = 15f;
 
     public override void UseAbility (Vector3 mousePos)
     {
         // add healing effects
-        Tower t = GameObject.Find ("TargetFinder").GetComponent<TowerTargets> ().GetTarget (mousePos);
-        if (t.Allegiance == Allegiance.Rodelle)
-            t.Damage (-1 * t.MaxHealth);
+        if (mReadyToUse)
+        {
+            Tower t = GameObject.Find("TargetFinder").GetComponent<TowerTargets>().GetTarget(mousePos);
+            if (t.Allegiance == Allegiance.Rodelle)
+            {
+                t.Damage(-1 * t.MaxHealth);
+                mReadyToUse = false;
+                // Add method to activate cooldown bar
+                Invoke("SetReadyToUse", mCoolDown);
+            }
+        }
     }
 
     public override bool ValidMousePos (Vector3 mousePos)
     {
         mTarget = GameObject.Find ("TargetFinder").GetComponent<TowerTargets> ().ValidMousePos (mousePos);
         return mTarget > -1;
+    }
+
+    private void SetReadyToUse()
+    {
+        mReadyToUse = true;
     }
 
 }
