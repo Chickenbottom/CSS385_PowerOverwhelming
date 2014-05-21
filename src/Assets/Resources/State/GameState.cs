c60ec71efdb2c30231a2c21e25ff832fe7077079
James Murphree <jamesus95@gmail.com>
2014-05-21 13:55:43 -0700

    getting win/lose to work;

--- src/Assets/Resources/State/GameState.cs
+++ src/Assets/Resources/State/GameState.cs
@@ -56,18 +56,18 @@ public class GameState
     public static void TriggerLoss ()
     {
 		LostGame = true;
-		//GameObject.Find("LoseFrame").GetComponent<LoseGame>().LostGame();
-        Application.LoadLevel("LevelLoader");
+        GameObject.Find("LoseFrame").GetComponent<LoseGame>().LostGame();
+        //Application.LoadLevel("LevelLoader");
     }
     
     public static void TriggerWin ()
     {
 		WonGame = true;
-		//GameObject.Find("WinFrame").GetComponent<WinGame>().WonGame();
+        GameObject.Find("WinFrame").GetComponent<WinGame>().WonGame();
         //Debug.Log ("You win!");
-        //GUIText text = GameObject.Find ("DialogueLeft").GetComponent<GUIText> ();
-        //text.text = "The peasants are gone!";
-        Application.LoadLevel("LevelLoader");
+        GUIText text = GameObject.Find("DialogueLeft").GetComponent<GUIText>();
+        text.text = "The peasants are gone!";
+        //Application.LoadLevel("LevelLoader");
     }
     
     public static void UpdateKingsHealth (int value)
