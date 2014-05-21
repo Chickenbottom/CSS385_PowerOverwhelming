5fdb85a0d600f2aa6e03bd99c33c7a8ae8e95287
Chickenbottom <chad.hickenbottom@gmail.com>
2014-05-21 13:06:31 -0700

    GTS for Playtest

--- src/Assets/Scenes/GalaticTowerStore/ButtonScripts/Store/SelectBar.cs
+++ src/Assets/Scenes/GalaticTowerStore/ButtonScripts/Store/SelectBar.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 public class SelectBar : MonoBehaviour {
 
-	public TowerStoreBehavior mStore;
-	public BonusSubject mBonusSubject;
-	public GUIText CoolDownText, SpawnRateText, SpawnSizeText, TowerHealthText;
+	//public TowerStoreBehavior mStore;
+	//public BonusSubject mBonusSubject;
+	//public GUIText CoolDownText, SpawnRateText, SpawnSizeText, TowerHealthText;
 
-	int cooldown, spawnrate, spawnsize, towerhealth;
+	//int cooldown, spawnrate, spawnsize, towerhealth;
 
-	void OnMouseDown(){
+	public List<GameObject> mAllTabs;
+	GameObject mCurrentTab;
+
+	public void SetTab(GameObject CurTab){
+		for(int i = 0; i < mAllTabs.Count; i++){
+			GameObject temp = (GameObject)mAllTabs[i];
+			temp.renderer.sortingOrder = 1;
+		}
+		mCurrentTab = CurTab;
+		mCurrentTab.renderer.sortingOrder = 10;
+				//mCurrentTab.layer = 11;
+		/*
 		mStore.mCurBonusSubject = mBonusSubject;
 
 		cooldown = ConvertBonusToInt(mStore.DynamicUpgrades[(int)mBonusSubject, (int)BonusType.CoolDown]);	
@@ -24,6 +35,7 @@ public class SelectBar : MonoBehaviour {
 
 		towerhealth = ConvertBonusToInt(mStore.DynamicUpgrades[(int)mBonusSubject, (int)BonusType.Health]);
 		TowerHealthText.text =  towerhealth.ToString();
+		*/
 	}
 	public static int ConvertBonusToInt(float percent){
 		percent -= 1;
--- src/Assets/Scenes/GalaticTowerStore/ButtonScripts/Store/SpawnSize/SpawnSizeDownButton.cs
+++ src/Assets/Scenes/GalaticTowerStore/ButtonScripts/Store/SpawnSize/SpawnSizeDownButton.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class SpawnSizeDownButton : SpawnSize {
 
+
 	void OnMouseDown(){
-		if( ( BonusLevel - 1) > 0 || ( BonusLevel - 1 ) > GetOriginal())  
-		NewValue(-1);
+		if( ( BonusLevel - 1) > 0 || ( BonusLevel - 1 ) > GetOriginal())  {
+			int temp = int.Parse(mTotalGold.text);
+			temp -= 15;
+			mTotalGold.text = temp.ToString();
+			NewValue(-1);
+		}
 	}
 
 }
