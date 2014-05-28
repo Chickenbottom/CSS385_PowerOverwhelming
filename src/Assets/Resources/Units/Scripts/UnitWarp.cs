ff430e13683c574a69018f9110f4fe55885ef603
Rodelle Ladia Jr <rLadia@gmail.com>
2014-05-27 21:48:05 -0700

    Units display a warp when they die
    Peasants are transported to the Galactic Tower Store when they die.

---
+++ src/Assets/Resources/Units/Scripts/UnitWarp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitWarp : MonoBehaviour {
+    
+    public void DestroyThis()
+    {
+        Destroy (this.gameObject);
+        Destroy (this);
+    }
+    
+	// Use this for initialization
+	void Start () {
+        Invoke("DestroyThis", 1f);
+	}
+}
