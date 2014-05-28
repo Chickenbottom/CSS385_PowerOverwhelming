7fb026d27ce56a7ad785c7929dd95b5a4029324f
James Murphree <jamesus95@gmail.com>
2014-05-28 15:17:00 -0700

    beta test

--- src/Assets/Resources/Menus/GameMenu/WinLoseMenu/WinLoseReplayButton.cs
+++ src/Assets/Resources/Menus/GameMenu/WinLoseMenu/WinLoseReplayButton.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEditor;
+//using UnityEditor;
 
 public class WinLoseReplayButton : ButtonBehaviour {
 	void OnMouseDown(){
-		string[] path = EditorApplication.currentScene.Split(char.Parse("/"));
-		string[] pathEnd = path[path.Length-1].Split(char.Parse("."));
+		//string[] path = UnityEditor.EditorApplication.currentScene.Split(char.Parse("/"));
+		//string[] pathEnd = path[path.Length-1].Split(char.Parse("."));
 
 
 		GameState.WonGame = GameState.LostGame = false;
-		Application.LoadLevel(pathEnd[0]);
+		Application.LoadLevel("LevelLoader");
 	}
 }
