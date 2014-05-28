42062916386f23c2331046c1d8abff5f12151f74
Rodelle Ladia Jr <rLadia@gmail.com>
2014-05-27 23:58:25 -0700

    Code formatting

--- src/Assets/Resources/UI/Dialogue.cs
+++ src/Assets/Resources/UI/Dialogue.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 // who is saying what, and where
-public struct Message 
+public struct Message
 {
     public float Duration;
     public Speaker Who;
@@ -16,15 +16,14 @@ public class Dialogue
 {   
     private const float kLetterDisplayTime = .05f;
     public bool IsDone = false;
-    
     Queue<Message> mMessages;
     
-    public Dialogue()
+    public Dialogue ()
     {
-        mMessages = new Queue<Message>();
+        mMessages = new Queue<Message> ();
         
         // dummy object that is removed on the first call to AdvanceMessage
-        mMessages.Enqueue (new Message()); 
+        mMessages.Enqueue (new Message ()); 
         mDialogueTimer = 0;
     }
     
@@ -35,10 +34,10 @@ public class Dialogue
     /// <c>true</c>, if the message was successfully updated.</returns>
     /// <param name="deltaTime">Delta time. The time since the last AdvanceMessage call.</param>
     /// <param name="message">Message. Returned containing information on how to display the message.</param>
-    public bool AdvanceMessage(float deltaTime, out Message message)
+    public bool AdvanceMessage (float deltaTime, out Message message)
     {
         if (IsDone) {
-            message = new Message();
+            message = new Message ();
             return false;
         }
         
@@ -46,18 +45,18 @@ public class Dialogue
         
         // The current message is finished, advance to the next one
         if (mDialogueTimer > mTimeUntilNextMessage) { 
-            mMessages.Dequeue(); // the current message is finished
+            mMessages.Dequeue (); // the current message is finished
             
             if (mMessages.Count > 0) {
-                SetupNextMessage();
+                SetupNextMessage ();
             } else {
                 IsDone = true;
-                message = new Message();
+                message = new Message ();
                 return false;
             }
         }
         
-        AdvanceLetterAnimation();
+        AdvanceLetterAnimation ();
         message = mCurrentMessage;
         return true;
     }
@@ -69,16 +68,16 @@ public class Dialogue
     private int mStringLength; // used to display the message one character at a time
     private float mPreviousLetterTime;
     
-    private void SetupNextMessage()
+    private void SetupNextMessage ()
     {
-        mCurrentMessage = mMessages.Peek();
+        mCurrentMessage = mMessages.Peek ();
         mTimeUntilNextMessage = mCurrentMessage.Duration;
         mCurrentText = mCurrentMessage.Text;
         mDialogueTimer = 0;
         mStringLength = 1;
     }
     
-    private void AdvanceLetterAnimation()
+    private void AdvanceLetterAnimation ()
     {
         if (mStringLength >= mCurrentText.Length || Time.time - mPreviousLetterTime < kLetterDisplayTime)        
             return;
@@ -89,29 +88,29 @@ public class Dialogue
         // TODO only add newlines between words, get rid of extra space after newline
         // TODO dynamically calculate this width based on the font?
         if (mStringLength % 60 == 0) { // add a newline every 60 characters
-            string modifiedText = mCurrentText.Substring(0, mStringLength);
+            string modifiedText = mCurrentText.Substring (0, mStringLength);
             modifiedText += "\n";
             mCurrentText = modifiedText + 
-                mCurrentText.Substring(mStringLength, mCurrentText.Length - mStringLength);            
+                mCurrentText.Substring (mStringLength, mCurrentText.Length - mStringLength);            
         }
         
-        mCurrentMessage.Text = mCurrentText.Substring(0, mStringLength);
+        mCurrentMessage.Text = mCurrentText.Substring (0, mStringLength);
     }
     
     // Returns false to indicate that there is no valid message
-    private bool EmptyDialogue(out Message message)
+    private bool EmptyDialogue (out Message message)
     {
         IsDone = true;
-        message = new Message();
+        message = new Message ();
         return false;
     }
     
     // when, how, who, where, what
     // ex. for <5> seconds, <Nervous> <King> (<Left> side), says <"Hello world!">, 
     //     for <3.2> seconds, <Normal> <Peasant> (<Right> side), says <"....blargh.">
-    public void AddMessage(float duration, SpeakerState state, Speaker speaker, SpeakerLocation location, string text)
+    public void AddMessage (float duration, SpeakerState state, Speaker speaker, SpeakerLocation location, string text)
     {
-        Message message = new Message();
+        Message message = new Message ();
         message.Duration = duration;
         message.Who = speaker;
         message.Text = text;
@@ -120,4 +119,36 @@ public class Dialogue
         
         mMessages.Enqueue (message);
     }
+    
+    /*private string FormatString (string text, int textWidth)
+    { 
+        string[] words = text.Split(" "); //Split the string into seperate words 
+        
+        GUIText t = new GUIText();
+        
+        string result = ""; 
+        for (var index = 0; index < words.Length; index++) { 
+            var word = words[index].Trim();
+            
+            if (index == 0) { 
+                result = words[0]; 
+                guiText.text = result; 
+            } 
+            
+            if (index > 0) { 
+                result += " " + word; 
+                guiText.text = result; 
+            } 
+            
+            TextSize = guiText.GetScreenRect(); 
+            
+            if (TextSize.width > lineLength)
+            {
+                result = result.Substring(0,result.Length-(word.Length)); 
+                result += "\n" + word; 
+                numberOfLines += 1; 
+                guiText.text = result; 
+            } 
+        } 
+    }*/
 }
