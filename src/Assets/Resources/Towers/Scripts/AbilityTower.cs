6d216f8048a3fb3d11be594ef43f4adcf4b8bbe2
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-05-29 16:27:57 -0700

    Added cooldown bars to AbilityTowers

--- src/Assets/Resources/Towers/Scripts/AbilityTower.cs
+++ src/Assets/Resources/Towers/Scripts/AbilityTower.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class AbilityTower: Tower
 {
-    public Ability ability;    
+    public Ability ability;
+    public Progressbar CooldownBar;
 
     public override void UseTargetedAbility (Target target)
     {
@@ -17,4 +18,17 @@ public class AbilityTower: Tower
     {
         ability.UsePositionalAbility(destination);
     }
+    
+    protected virtual void Update()
+    {
+        CooldownBar.UpdateValue((int)(ability.CooldownTimer));
+    }
+    
+    protected virtual void Start ()
+    {
+        base.Awake ();
+        CooldownBar.MaxValue = (int)(ability.CoolDown);
+        CooldownBar.UpdateValue((int)(ability.CooldownTimer));
+    }
+    
 }
