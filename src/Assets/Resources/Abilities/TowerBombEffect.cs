296ea1e95cd698fc45f272e0e23ea8674d6f8d24
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-06-07 04:17:55 -0700

    Replaced Aoe particle effect with portal animation
    The particle effects don't match up well with the game's art style.

--- src/Assets/Resources/Abilities/TowerBombEffect.cs
+++ src/Assets/Resources/Abilities/TowerBombEffect.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -9,35 +9,29 @@ public class TowerBombEffect : MonoBehaviour
         mDamage = damage;
         this.transform.position = location;
         
-        int sortingOrder = (int)(4 * (-this.transform.position.y + Camera.main.orthographicSize));
-        this.GetComponent<Renderer> ().sortingOrder = (int)(sortingOrder);        
-        
-        kStartTime = Time.time;
-        mRadius = this.GetComponent<CircleCollider2D>().radius;
+        mStartTime = Time.time;
     }
-    
-    // TODO replace with states
+
     private enum ExplosionState {
         Idle,
         Started,
         Finished,
     }
+    
     private ExplosionState mExplosionState = ExplosionState.Idle;
     
     float kLiveTimer = 1.8f;
-    float kStartTime;
-
-    GameObject mEmitterPrefab = null;
+    float mStartTime;
     
     private int mDamage;
-    private float mRadius;
     
     void Update ()
     {
-        if (Time.time - kStartTime > kLiveTimer) {
-            Destroy (mEmitterPrefab.gameObject);
+        if (Time.time - mStartTime > kLiveTimer) {
             Destroy (this.gameObject);
         }
+        
+        this.transform.Rotate(new Vector3(0, 0, 100f * Time.smoothDeltaTime));
     }
     
     void OnTriggerEnter2D (Collider2D other)
@@ -54,43 +48,13 @@ public class TowerBombEffect : MonoBehaviour
         
         Target target = other.gameObject.GetComponent<Target> ();
         
-        if (target == null || ! (target is Unit))
-            return;
-        
-        this.Knockback (target);
+        if (target is Unit)
+            target.Damage (mDamage);
     }
    
     void FixedUpdate()
     {
         if (mExplosionState == ExplosionState.Started)
             mExplosionState = ExplosionState.Finished;
-    }
-    
-    private void Knockback(Target target)
-    {
-        if (! (target is Unit))
-            return;
-        
-        List<Unit> squad = target.GetComponent<Unit> ().Squad.SquadMembers;
-        
-        for (int i = 0; i < squad.Count; ++i) {
-            Unit u = squad[i];
-            Vector3 toCenter = u.Position - this.transform.position;
-            
-            float knockbackDistance = toCenter.magnitude;
-            float percentFromCenter = (1 - knockbackDistance / mRadius);
-            knockbackDistance = percentFromCenter * mRadius;
-            knockbackDistance /= 3f;
-            
-            toCenter.Normalize();
-            u.Position += toCenter * knockbackDistance;
-            
-            // Deals 50-100% damage depending on distance from center
-            float percentDamage = Mathf.Max(1f, percentFromCenter + 0.5f); 
-            
-            target.Damage((int)(mDamage * percentDamage));
-            
-            u.BuffMovement(0f, 2f); // stun for 2 seconds
-        }
     }
 }
